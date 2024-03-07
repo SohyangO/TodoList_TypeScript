e5f940b293595e58cbe09af485eef776a5a7fbdf
SohyangO <shyang903@naver.com>
2024-03-07 16:03:50 +0900

    Add: 뼈대 잡기

---
+++ src/components/AddForm.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function AddForm() {
+  return (
+    <div>
+      <form>
+        <input placeholder="제목"></input>
+        <input placeholder="내용"></input>
+        <button>등록하기</button>
+      </form>
+    </div>
+  );
+}
+
+export default AddForm;
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <h1>My Todo list</h1>
+      <p>React</p>
+    </div>
+  );
+}
+
+export default Header;
