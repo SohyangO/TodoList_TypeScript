ab113da556b28b6900790e59e3451542869d64ea
SohyangO <shyang903@naver.com>
2024-03-07 16:50:54 +0900

    Add:등록하기 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,49 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import AddForm from "./components/AddForm";
+import { v4 } from "uuid";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+type Todo = {
+  id: string;
+  title: string;
+  content: string;
+  isDone: boolean;
+};
 
+function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodo = (title: string, content: string) => {
+    const newTodo: Todo = {
+      id: v4(),
+      title,
+      content,
+      isDone: false,
+    };
+    const newTodos = [...todos, newTodo];
+    setTodos(newTodos);
+  };
   return (
     <>
       <div>
         <Header />
-        <AddForm></AddForm>
+        <AddForm addTodo={addTodo} />
       </div>
       <div>
         <h1>Is progress</h1>
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              <div>
+                <p>{todo.title}</p>
+                <p>{todo.content}</p>
+                <button>{todo.isDone ? "완료" : "취소"}</button>
+                <button>삭제</button>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
       <div>
         <h1>Done</h1>
--- src/components/AddForm.tsx
+++ src/components/AddForm.tsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
-function AddForm() {
+function AddForm({
+  addTodo,
+}: {
+  addTodo: (title: string, content: string) => void;
+}) {
+  const [title, setTitle] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+
+  const onSubmitTitleHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    //제목과 내용으로 할 일 추가
+    addTodo(title, content);
+    setTitle("");
+    setContent("");
+  };
   return (
     <div>
-      <form>
-        <input placeholder="제목"></input>
-        <input placeholder="내용"></input>
-        <button>등록하기</button>
+      <form onSubmit={onSubmitTitleHandler}>
+        <input
+          placeholder="제목"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        ></input>
+        <input
+          placeholder="내용"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        ></input>
+        <button type="submit">등록하기</button>
       </form>
     </div>
   );
