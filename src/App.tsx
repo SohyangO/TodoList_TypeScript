1a2b132b41b6a9fd2660a45daa297b54fc09b3e0
SohyangO <shyang903@naver.com>
2024-03-07 18:28:55 +0900

    Add: 완료, 취소 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -34,6 +34,16 @@ function App() {
     setTodos(deletedTodo);
   };
 
+  const onCancelButtonHandler = (id: string) => {
+    const canceled = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isDone = !todo.isDone;
+      }
+      return todo;
+    });
+    setTodos([...canceled]);
+  };
+
   return (
     <>
       <div>
@@ -43,27 +53,54 @@ function App() {
       <div>
         <h1>Is progress</h1>
         <ul>
-          {todos.map((todo) => (
-            <li key={todo.id}>
-              <div>
-                <p>{todo.title}</p>
-                <p>{todo.content}</p>
-                <p>
-                  {new Date(todo.deadLine).toLocaleDateString("ko-KR", {
-                    year: "numeric",
-                    month: "long",
-                    day: "numeric",
-                  })}
-                </p>
-                <button>{todo.isDone ? "완료" : "취소"}</button>
-                <DeleteTodo todoId={todo.id} onDelete={deleteTodo} />
-              </div>
-            </li>
-          ))}
+          {todos.map((todo) =>
+            !todo.isDone ? (
+              <li key={todo.id}>
+                <div>
+                  <p>{todo.title}</p>
+                  <p>{todo.content}</p>
+                  <p>
+                    {new Date(todo.deadLine).toLocaleDateString("ko-KR", {
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                    })}
+                  </p>
+                  <button onClick={() => onCancelButtonHandler(todo.id)}>
+                    완료
+                  </button>
+                  <DeleteTodo todoId={todo.id} onDelete={deleteTodo} />
+                </div>
+              </li>
+            ) : null
+          )}
         </ul>
       </div>
       <div>
         <h1>Done</h1>
+        <ul>
+          {todos.map((todo) =>
+            todo.isDone ? (
+              <li key={todo.id}>
+                <div>
+                  <p>{todo.title}</p>
+                  <p>{todo.content}</p>
+                  <p>
+                    {new Date(todo.deadLine).toLocaleDateString("ko-KR", {
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                    })}
+                  </p>
+                  <button onClick={() => onCancelButtonHandler(todo.id)}>
+                    취소
+                  </button>
+                  <DeleteTodo todoId={todo.id} onDelete={deleteTodo} />
+                </div>
+              </li>
+            ) : null
+          )}
+        </ul>
       </div>
     </>
   );
